4a89556f3bbfe8e20eb7dfa25427b36ed97080da
Hufan <hufany21@gmail.com>
2021-12-12 14:56:33 -0500

    If I use this commit, it will fix counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,38 +1,37 @@
 
-
-
- $(document).ready(function() {
+$(document).ready(function () {
   const $text = document.getElementById("counter")
 
-  let i = 140
+  let count = 140
   const updateDisplay = () => {
-  document.getElementById("counter").innerHTML = i;
-};
+    document.getElementById("counter").innerHTML = i;
+  };
 
-  $("#tweet-text").on('keydown', function(event) {
-    if(i <= 0){    // if char under 0, turn counter red
+  $("#tweet-text").on('keyup', function (event) {
+
+
+    count = 140 - $(this).val().length;
+
+    if (event.key === "Backspace") {
+      i + 1;
+    }
+
+    if ($(this).val().length > 139) {    // if char under 0, turn counter red
       $text.style.color = "red"
-      i--;
-     updateDisplay();
-    } 
-    const key = event.key;
 
-    if (key === "Backspace") { // attempt at counter reversing when letters deleted
-      i++
       updateDisplay();
     }
 
     else {
-    i--;
-    updateDisplay();
-    }
-    $("form").on("submit", function (event) {    // on submit, refresh counter and make it black
       $text.style.color = "black"
+      updateDisplay();
+    }
+
+    $("form").on("submit", function (event) {    // on submit, refresh counter and make it black
       return i = 140
     })
-   
-    })
 
-  
+  })
+
+
 });
- 
